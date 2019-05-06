cdc32489fa2d3523249af46bf38bb180ba512b6d
nikolas-n <nikolas@zmail.com>
2019-05-06 21:38:21 +0300

    Latest

--- live/getarrivals.sh
+++ live/getarrivals.sh
@@ -8,16 +8,22 @@ BASEURL="http://telematics.oasa.gr/api/?act="
 ACTION="getStopArrivals"
 STOP=$1
 
-curl -s "http://telematics.oasa.gr/api/?act=getStopNameAndXY&p1=$STOP" | jq -r '.[].stop_descr'
+echo "<span><h4>"
+curl -s "http://telematics.oasa.gr/api/?act=getStopNameAndXY&p1=$STOP" | jq -r '.[].stop_descr' 2>/dev/null
+echo "</h4>"
 
-echo "========"
-
-thecurl=$(curl -s "$BASEURL$ACTION" --data "p1=$STOP" |  jq  -r 'map(.route_code,",",.veh_code,",",.btime2,"\n") | join ("")' | awk NF)
+thecurl=$(curl -s "$BASEURL$ACTION" --data "p1=$STOP")
+if [[ ! -z $thecurl ]]
+then
+thecurl=$(echo "$thecurl" | jq  -r 'map(.route_code,",",.veh_code,",",.btime2,"\n") | join ("")' | awk NF)
 
 IFS=","
 while read -r route vehicle time; do
-  route=$(grep "$route" "/home/nik/projects/oasa/OASA-telematics-playground/routes" | awk -F "," '{print $4}')
-  my+="$route : $time´
+  route=$(grep "$route" "/home/nik/projects/oasa/OASA-telematics-playground/routes" | awk -F "," '{print $3}')
+  my+="$route : $time<sup><small>$vehicle</small></sup>
 "
 done <<< "$thecurl"
-echo "$my" |  awk -F':' -v OFS='' '{x=$1;$1="";a[x]=a[x]$0}END{for(x in a)print x,":"a[x]}' | grep -v ":$"
+echo "$my" |  awk -F':' -v OFS='' '{x=$1;$1="";a[x]=a[x]$0}END{for(x in a)print x,":"a[x]}' | grep -v ":$" | sed 's/$/<br>/g'
+echo "</span>"
+
+fi
